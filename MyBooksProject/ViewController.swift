a111da7956995b87bc80e1e747c1bd8450c99412
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-10-04 19:39:38 +0300

    Initial Commit

---
+++ MyBooksProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBooksProject
+//
+//  Created by Ozan Çiçek on 4.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
